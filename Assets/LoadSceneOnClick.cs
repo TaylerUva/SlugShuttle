9b6c13f2fe06035ff1efe92984d928ea1c73f49a
Tayler Uva <tayleruva@gmail.com>
2018-08-03 11:19:53 -0700

    added menu scene

---
+++ Assets/LoadSceneOnClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+    public void LoadByIndex(int sceneIndex) {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
