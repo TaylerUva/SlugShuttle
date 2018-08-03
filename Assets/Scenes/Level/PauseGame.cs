a39311125760024783cf70cf9c9c53e812be7856
Tayler Uva <tayleruva@gmail.com>
2018-08-03 16:49:18 -0700

    added game over
    not yet active

--- Assets/Scenes/Level/PauseGame.cs
+++ Assets/Scenes/Level/PauseGame.cs
@@ -5,23 +5,16 @@ using UnityEngine.UI;
 
 
 public class PauseGame : MonoBehaviour {
-
-    private bool isPaused = false;
+    
     public GameObject pauseMenu;
-
-    void Start() {
-        Resume();
-    }
 
     public void Pause(){
         Time.timeScale = 0;
-        isPaused = true;
         pauseMenu.SetActive(true);
     }
 
     public void Resume(){
         Time.timeScale = 1;
-        isPaused = false;
         pauseMenu.SetActive(false);
     }
 }
