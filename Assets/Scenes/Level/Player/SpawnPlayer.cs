09727d7da5df59a79cfd368cd018a393c0c959a1
Tayler Uva <tayleruva@gmail.com>
2018-08-03 22:10:21 -0700

    added game over screen

--- Assets/Scenes/Level/Player/SpawnPlayer.cs
+++ Assets/Scenes/Level/Player/SpawnPlayer.cs
@@ -8,12 +8,17 @@ public class SpawnPlayer : MonoBehaviour {
     public GameObject playerObject;
     public int lives = 3;
     public Text textObject;
+    public GameObject gameOverMenu;
+    private bool isGameOver = false;
+    private bool wasLifeLost = false;
     private GameObject playerInstance;
     private float respawnTimer;
 
     // Use this for initialization
     void Start() {
-        textObject.text = "Lives: " + ++lives;
+        textObject.text = "Lives: " + lives;
+        //isGameOver = false;
+        //gameOver.SetActive(false);
         Spawn();
     }
 
@@ -21,18 +26,31 @@ public class SpawnPlayer : MonoBehaviour {
         respawnTimer = 1;
         playerInstance = (GameObject)Instantiate(playerObject);
         playerInstance.name = "Player";
-        textObject.text = "Lives: " + --lives;
+        wasLifeLost = false;
+    }
+
+    bool DoneRespawning(){
+        respawnTimer -= Time.deltaTime;
+        return (respawnTimer <= 0);
+    }
+
+    void GameOver(){
+        isGameOver = true;
+        Time.timeScale = 0;
+        gameOverMenu.SetActive(true);
     }
 
     // Update is called once per frame
     void Update() {
-        if (playerInstance == null) {
-            respawnTimer -= Time.deltaTime;
-            if (lives != 0) {
-                if (respawnTimer <= 0) Spawn();
+        if (lives >= 0 && playerInstance == null && !isGameOver){
+            if (!wasLifeLost) {
+                --lives;
+                wasLifeLost = true;
+                if (lives >= 0) textObject.text = "Lives: " + lives;
+                else GameOver();
             }
-            else{
-                Debug.Log("GAME OVER!");
+            if (DoneRespawning()){
+                Spawn();
             }
         }
     }
