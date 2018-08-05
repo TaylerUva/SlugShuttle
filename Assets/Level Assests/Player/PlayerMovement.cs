a7ff451512602d76ce85d07caee07c19dec662d5
Tayler Uva <tayleruva@gmail.com>
2018-08-05 12:27:17 -0700

    added screen bounds

--- Assets/Level Assests/Player/PlayerMovement.cs
+++ Assets/Level Assests/Player/PlayerMovement.cs
@@ -6,9 +6,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public bool yAxisEnabled = true;
+    private float spriteWidth;
     private int maxSpeed = 10;
     private float moveX;
     private float moveY;
+
+    private void Start() {
+        spriteWidth = gameObject.GetComponent<SpriteRenderer>().bounds.size.x;
+    }
 
     // Update is called once per frame
     void Update() {
@@ -20,6 +25,8 @@ public class PlayerMovement : MonoBehaviour {
         //CONTROLS
         pos.x += Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime;
         if (yAxisEnabled) pos.y += Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime;
+        if ((pos.x + spriteWidth/2) > Camera.main.orthographicSize) pos.x = -Camera.main.orthographicSize + spriteWidth/2;
+        if ((pos.x - spriteWidth/2) < -Camera.main.orthographicSize) pos.x = Camera.main.orthographicSize - spriteWidth/2;
 
         transform.position = pos;
     }
