aad09ea9802226341f75a9c9e34335d0b022e773
Tayler Uva <tayleruva@gmail.com>
2018-08-05 22:16:02 -0700

    added temp fix to highscore bug

--- Assets/Gameplay Assets/DamageHandler.cs
+++ Assets/Gameplay Assets/DamageHandler.cs
@@ -9,7 +9,7 @@ public class DamageHandler : GameplayEventSystem {
     private int invulnLayer = 10;
     private int originalLayer;
     private Text healthText;
-    public bool isPlayer = false;
+    private bool isPlayer = false;
     public int health = 1;
 
     // Use this for initialization
@@ -17,6 +17,7 @@ public class DamageHandler : GameplayEventSystem {
         EventSystemInit();
         originalLayer = gameObject.layer;
         healthText = GameObject.Find("Canvas").transform.Find("Health").GetComponent<Text>();
+        isPlayer = gameObject.CompareTag("Player");
         if (isPlayer) {
             health -= PlayerPrefs.GetInt("difficulty", 1) - 1;
             healthText.text = "Health: " + health;
@@ -43,7 +44,11 @@ public class DamageHandler : GameplayEventSystem {
             gameObject.GetComponent<SpriteRenderer>().enabled = !gameObject.GetComponent<SpriteRenderer>().enabled;
         }
         if (health <= 0) {
-            if (gameObject.CompareTag("Torpedo")) ScoreSystem().RaiseScore(); //!!!!!! THIS IS A TEMP FIX TO AVOID HIGHSCORE BUGS
+            if (gameObject.CompareTag("Torpedo")) {
+                //!!!!!! THIS IS A TEMP FIX TO AVOID HIGHSCORE BUGS
+                ScoreSystem().RaiseScore();
+                Debug.LogWarning("THIS IS A TEMP FIX TO AVOID HIGHSCORE BUGS");
+            }
             if (isPlayer) GameOverSystem().GameOver();
             Die();
         }
