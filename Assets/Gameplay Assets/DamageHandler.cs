5c387941590eefaf5e9ec5cc3b745bfb9bc6018c
Tayler Uva <tayleruva@gmail.com>
2018-08-05 21:39:05 -0700

    converted player life management
    made it health based with the DamageHandler class

--- Assets/Gameplay Assets/DamageHandler.cs
+++ Assets/Gameplay Assets/DamageHandler.cs
@@ -1,25 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DamageHandler : GameplayEventSystem {
 
     private float invulnTimer = 0;
     private int invulnLayer = 10;
     private int originalLayer;
+    private Text healthText;
+    public bool isPlayer = false;
     public int health = 1;
 
     // Use this for initialization
     void Start() {
         EventSystemInit();
         originalLayer = gameObject.layer;
+        healthText = GameObject.Find("Canvas").transform.Find("Health").GetComponent<Text>();
+        if (isPlayer) {
+            health -= PlayerPrefs.GetInt("difficulty", 1) - 1;
+            healthText.text = "Health: " + health;
+        }
     }
 
     private void OnTriggerEnter2D() {
         health--;
         invulnTimer = 0.5f;
-
         gameObject.layer = invulnLayer;
+        if (isPlayer){
+            healthText.text = "Health: " + health;
+        }
     }
 
     // Update is called once per frame
@@ -33,7 +43,8 @@ public class DamageHandler : GameplayEventSystem {
             gameObject.GetComponent<SpriteRenderer>().enabled = !gameObject.GetComponent<SpriteRenderer>().enabled;
         }
         if (health <= 0) {
-            if (gameObject.CompareTag("Torpedo")) ScoreSystem().RaiseScore();
+            if (gameObject.CompareTag("Torpedo")) ScoreSystem().RaiseScore(); //!!!!!! THIS IS A TEMP FIX TO AVOID HIGHSCORE BUGS
+            if (isPlayer) GameOverSystem().GameOver();
             Die();
         }
     }
