6955db53cf42bf4517fbb721604be4637022e724
Tayler Uva <tayleruva@gmail.com>
2018-08-06 22:27:52 -0700

    added background music

---
+++ Assets/KeepBGMAlive.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeepBGMAlive : MonoBehaviour {
+
+    void Awake() {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("DontDestroyMusic");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+
+    void Update() {
+        // Change BGMusic
+        //if (SceneManager.GetActiveScene().name == "SceneName") {
+        //    Destroy(this.gameObject);
+        //}
+    }
+}
