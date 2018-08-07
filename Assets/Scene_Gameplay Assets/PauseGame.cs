67aaa6a896a4743be81029dc6b3bbdda06fe853b
Tayler Uva <tayleruva@gmail.com>
2018-08-07 01:03:57 -0700

    added effects volume slider to pause game
    updated sounds

--- Assets/Scene_Gameplay Assets/PauseGame.cs
+++ Assets/Scene_Gameplay Assets/PauseGame.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class PauseGame : MonoBehaviour {
     
     public GameObject pauseMenu;
+    public GameObject pauseButton;
 
     public void Pause(){
         Time.timeScale = 0;
+        pauseButton.SetActive(false);
         pauseMenu.SetActive(true);
     }
 
     public void Resume(){
         Time.timeScale = 1;
+        pauseButton.SetActive(true);
         pauseMenu.SetActive(false);
     }
 }
