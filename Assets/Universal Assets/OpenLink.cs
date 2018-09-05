9e8b8f8f498307b689f742f23d806c16e1452528
Tayler Uva <tayleruva@gmail.com>
2018-09-04 19:02:06 -0700

    WEB UPDATE: Created version 1.0

---
+++ Assets/Universal Assets/OpenLink.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour {
+
+    public void OpenWebsite() {
+        Application.OpenURL("https://Tayler.Tech");
+    }
+}
